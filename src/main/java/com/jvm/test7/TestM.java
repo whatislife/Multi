6d66ef41a1a0f80f96c45a86f67e099cda0c71e4
whatislife <1053400192@qq.com>
2020-03-03 15:32:29 +0800

    提交垃圾回收部分代码

---
+++ src/main/java/com/jvm/test7/TestM.java
@@ -0,0 +1,16 @@
+package com.jvm.test7;
+/**
+ * 
+* @ClassName: TestM  
+* <p>Description: 垃圾回收  </p>
+* @author  frankman
+* @date 2020年3月3日 下午3:32:04  
+*
+ */
+public class TestM {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
